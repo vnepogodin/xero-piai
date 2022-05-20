5424abe4e9c71b0cc55640463deb276971c40115
XeroLinux <steve@techxero.com>
2022-05-20 15:30:05 +0300

    Paccache

--- src/pacmancache.cpp
+++ src/pacmancache.cpp
@@ -52,7 +52,6 @@ QString PacmanCache::getArch() {
     // Pair of arch names returned by "uname" and corresponding DEB_BUILD_ARCH formats
     static const std::unordered_map<QString, QString> arch_names{
         {"x86_64", "amd64"},
-        {"x86_64_v3", "amd64"},
         {"i686", "i386"},
         {"armv7l", "armhf"}};
 
